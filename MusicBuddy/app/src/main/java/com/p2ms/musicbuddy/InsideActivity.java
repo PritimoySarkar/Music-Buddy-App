497a4bf020ce9479d808217a0a39d82bfe9519d5
Pritimoy Sarkar <pritimoysarkar@gmail.com>
2020-09-03 03:27:26 +0530

    added bugs to fix latter

--- MusicBuddy/app/src/main/java/com/p2ms/musicbuddy/InsideActivity.java
+++ MusicBuddy/app/src/main/java/com/p2ms/musicbuddy/InsideActivity.java
@@ -1,11 +1,19 @@
 package com.p2ms.musicbuddy;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.p2ms.musicbuddy.local.LocalSession;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -13,6 +21,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class InsideActivity extends AppCompatActivity {
+    LocalSession session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +43,46 @@ public class InsideActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.logout_menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case (R.id.logout_buttonID):
+                Log.d("InsideActivity","Logout Button Clicked");
+                performLogout();
+                break;
+            case (R.id.Help_buttonID):
+                Log.d("InsideActivity","Search Button Clicked");
+                Toast.makeText(InsideActivity.this,"Opening App documentation",Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void performLogout() {
+        session = new LocalSession(InsideActivity.this);
+
+        final AlertDialog.Builder logoutAlert= new AlertDialog.Builder(InsideActivity.this);
+        logoutAlert.setMessage("Do you really want to logout");
+        logoutAlert.setCancelable(false);
+        logoutAlert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                session.cleanData();
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(InsideActivity.this,"Logged Out",Toast.LENGTH_LONG).show();
+                startActivity(new Intent(InsideActivity.this,LoginActivity.class));
+            }
+        });
+        logoutAlert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                Toast.makeText(InsideActivity.this,"Log Out canceled",Toast.LENGTH_LONG).show();
+            }
+        });
+        logoutAlert.show();
+        return;
     }
 }
